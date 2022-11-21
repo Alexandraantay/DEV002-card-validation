9a796ed1ab8e1ef8d94eadf995ace1203533d01c
Alexandra <antayalexandra@gmail.com>
2022-11-21 10:53:59 -0500

    Card Validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import validator from './validator.js';
+let registrarte = document.getElementById("registrarte");
+registrarte.addEventListener("click", MostrarVista2);
+function MostrarVista2() {
+    let vista_1 = document.getElementById("pantalla1")
+    pantalla1.style.display = "none";
+    let vista_2 = document.getElementById("pantalla2");
+    pantalla2.style.display = "inline-flex";
+    mostrarSaludo();
+}
 
-console.log(validator);
+function mostrarSaludo() {
+    let usuario = document.getElementById("name");
+    let nombre = usuario.value;
+
+    let hola = document.getElementById("saludo");
+
+    hola.innerHTML = "Hola, " + nombre + " ahora estarás registrando los datos de tu tarjeta para completar tu registro";
+}
+
+let inputCard = document.getElementById("numero");
+
+let btnValidation = document.getElementById("validar");
+btnValidation.addEventListener("click", function (e) {
+    e.preventDefault();
+    let numberCard = inputCard.value;
+    console.log(numberCard)
+    let resultValidation = validator.isValid(numberCard);
+    let resultMaskify = validator.maskify(numberCard);
+
+    if (resultValidation) {
+        document.getElementById("numero").innerHTML = resultMaskify;
+        alert("Felicidades ,la tarjeta ingresada es válida")
+    } else {
+        alert("La tarjeta registrada no es válida, por favor intente nuevamente")
+    }
+    inputCard.value= resultMaskify;
+})
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,49 @@
 const validator = {
-  // ...
-};
+  isValid: function (creditCardNumber) {
+    let reverseNumber = creditCardNumber.split("").reverse()
+    console.log("entro a is valid" + reverseNumber);
+    let result = 0;
+    for (let i = 0; i < reverseNumber.length; i++) {
+      if ((i + 1) % 2 === 0) {
+        let resultadopordos = reverseNumber[i] * 2
+        console.log("primer digito por dos" + resultadopordos)
+        if (resultadopordos >= 10) {
+          //arrayNumber[i]=arrayNumber[i].toString();
+          let resultadosuma = resultadopordos - 9;
+          console.log(reverseNumber[i], resultadosuma);
+          result += parseInt(resultadosuma)
+          console.log("suma de dígitos" + result)
+        }
+        else {
+          result += resultadopordos
+          console.log("digitos por dos" + result)
+        }
+      }
+      else {
+        result += parseInt(reverseNumber[i])
+        console.log("posición" + result)
+      }
+    }
+    console.log("resultado final" + result)
+    if (result % 10 === 0) {
+      return true;
+    }
+    else { return false }
+
+  },
+
+
+  maskify: function (creditCardNumber) {
+    console.log(creditCardNumber)
+    let lastNumbers = creditCardNumber.length;
+    let maskNumbers = "";
+
+    for (let i=0; i<lastNumbers-4; i++){
+      maskNumbers += "#";
+    }
+    return maskNumbers + creditCardNumber.substring(lastNumbers-4, lastNumbers)
+  }
+  }
+
 
 export default validator;
